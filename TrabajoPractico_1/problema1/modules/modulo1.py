a1725f92d11f5c9b33c3932da0b04f856b5f7833
LuchiCosta <lucianacosta3004@gmail.com>
2025-04-16 14:20:13 -0300

    empezamos las funciones

--- TrabajoPractico_1/problema1/modules/modulo1.py
+++
@@ -1,9 +0,0 @@
-# módulo para organizar funciones o clases utilizadas en nuestro proyecto
-# Crear tantos módulos como sea necesario para organizar el código
-
-def funcion():
-    print("funcion 1")
-    
-def funcion2():
-    print("funcion 2")
-
