37b890417858d41bc4a4c2a03f72db74b3a40bac
Mateo Anderson <mateo.anderson@ingenieria.uner.edu.ar>
2025-05-17 12:29:39 -0300

    arreglo tp 1 funcion extraer

--- TrabajoPractico_1/problema3/modules/ClaseLista.py
+++ TrabajoPractico_1/problema3/modules/ClaseLista.py
@@ -1,46 +1,45 @@
-# módulo para organizar funciones o clases utilizadas en nuestro proyecto
-# Crear tantos módulos como sea necesario para organizar el código
-class nodo: #permite recorrer la lista en ambos sentidos
+class nodo:
     def __init__(self, dato): 
-        self.dato = dato #almacena el dato
-        self.anterior = None #puntero al numero anterior
-        self.siguiente = None #puntero al numero siguiente
+        self.dato = dato
+        self.anterior = None
+        self.siguiente = None
+
+    def __str__(self):
+        return str(self.dato)
+
 
 class ListaDobleEnlazada:
     def __init__(self):
-        self.cabeza = None #aun no hay primer elemento
-        self.cola = None #tampoco hay ultimo elemento aun
-        self.tamanio = 0 #inicia en cero
-    
+        self.cabeza = None
+        self.cola = None
+        self.tamanio = 0
+
     def esta_vacia(self):
-        return self.cabeza is None #si devuelve true, esta vacia
+        return self.cabeza is None
     
     def __len__(self):
-        return self.tamanio #devuelve la cantidad elementos de la lista
+        return self.tamanio
 
     def agregar_al_inicio(self, item):
-        nuevo = nodo(item) #agregamos un nuevo nodo al inicio
-
+        nuevo = nodo(item)
         if self.esta_vacia():
             self.cabeza = self.cola = nuevo
-        
-        else: 
-            nuevo.siguiente = self.cabeza #nuevo nodo apunta a la cabeza actual
-            self.cabeza.anterior = nuevo #la cabeza actual apunta al nuevo nodo
-            self.cabeza = nuevo #la cabeza es el nuevo nodo
-        self.tamanio += 1 #aumenta el tamaño de la lista en 1 
+        else:
+            nuevo.siguiente = self.cabeza
+            self.cabeza.anterior = nuevo
+            self.cabeza = nuevo
+        self.tamanio += 1
 
     def agregar_al_final(self, item):
         nuevo = nodo(item)
-
         if self.esta_vacia():
             self.cabeza = self.cola = nuevo
-        else: 
+        else:
             self.cola.siguiente = nuevo
             nuevo.anterior = self.cola
             self.cola = nuevo
         self.tamanio += 1
-    
+
     def insertar(self, item, posicion):
         if posicion < 0 or posicion > self.tamanio:
             raise IndexError("Posición inválida")
@@ -53,78 +52,83 @@ class ListaDobleEnlazada:
             for _ in range(posicion):
                 actual = actual.siguiente
             nuevo = nodo(item)
-            anterior = actual.anterior #guardo el nodo anterior
-            anterior.siguiente = nuevo #ahora su nodo es el siguiente
-            nuevo.anterior = anterior #ahora sabe quien esta antes
-            nuevo.siguiente = actual #sabe cual es el siguiente
-            actual.anterior = nuevo 
+            anterior = actual.anterior
+            anterior.siguiente = nuevo
+            nuevo.anterior = anterior
+            nuevo.siguiente = actual
+            actual.anterior = nuevo
             self.tamanio += 1
 
-    def extraer(self, posicion = None): #me posiciono uno antes del elemento
+    def extraer(self, posicion=None):
         if self.tamanio == 0:
             raise IndexError("La lista está vacía")
         if posicion is None:
-            actual = self.cola
-            self.cola = self.cola.anterior
-            self.cola.siguiente = None
-        elif posicion == self.tamanio -1 or posicion == -1:
-            actual = self.cola
-            self.cola = actual.anterior
-            if self.cola:
-                self.cola.siguiente = None
-        elif posicion < 0 or posicion >= self.tamanio:
+            posicion = self.tamanio - 1
+        if posicion < 0:
+            posicion += self.tamanio
+        if posicion < 0 or posicion >= self.tamanio:
             raise IndexError("Posición inválida")
-        elif posicion == 0:
+
+        if self.tamanio == 1:
+            actual = self.cabeza
+            self.cabeza = self.cola = None
+            self.tamanio -= 1
+            return actual.dato
+        if posicion == 0:
             actual = self.cabeza
             self.cabeza = actual.siguiente
-            if self.cabeza:
-                self.cabeza.anterior = None
-        elif self.tamanio == 1:
-            actual = self.cabeza
-            self.cabeza = None
-            self.cola = None
-        if  posicion !=None  and posicion < (self.tamanio-1) and posicion>0:
-            actual = self.cabeza
-            for _ in range(posicion):
-                actual  = actual.siguiente
-        
-            actual.anterior.siguiente = actual.siguiente #recordar dibujito de la fila :)
-            actual.siguiente.anterior = actual.anterior 
-        
+            self.cabeza.anterior = None
+            self.tamanio -= 1
+            return actual.dato
+        if posicion == self.tamanio - 1:
+            actual = self.cola
+            self.cola = actual.anterior
+            self.cola.siguiente = None
+            self.tamanio -= 1
+            return actual.dato
+
+        actual = self.cabeza
+        for _ in range(posicion):
+            actual = actual.siguiente
+        actual.anterior.siguiente = actual.siguiente
+        actual.siguiente.anterior = actual.anterior
         self.tamanio -= 1
         return actual.dato
-    
+
     def copiar(self):
         copia = ListaDobleEnlazada()
         actual = self.cabeza
         while actual:
             copia.agregar_al_final(actual.dato)
-            actual = actual.siguiente #copia nodo por nodo, lo agrega a una nueva lista llamada copia
-        
+            actual = actual.siguiente
         return copia
-    
+
     def invertir(self):
         actual = self.cabeza
         while actual:
-            actual.anterior, actual.siguiente = actual.siguiente, actual.anterior #invierte el siguiente y el anterior
+            actual.anterior, actual.siguiente = actual.siguiente, actual.anterior
             actual = actual.anterior
-        self.cabeza, self.cola = self.cola, self.cabeza #intercambio primero y ultimo
+        self.cabeza, self.cola = self.cola, self.cabeza
 
-    def concatenar(self,otraLista):
-        copia = otraLista.copiar() #creo una copia de otraLista y la almaceno en copia
-        actual = copia.cabeza 
+    def concatenar(self, otraLista):
+        copia = otraLista.copiar()
+        actual = copia.cabeza
         while actual:
-            self.agregar_al_final(actual.dato) #agrega cada nodo al final de la actual
-            actual = actual.siguiente 
-    
+            self.agregar_al_final(actual.dato)
+            actual = actual.siguiente
+
     def __add__(self, otraLista):
         resultado = self.copiar()
         resultado.concatenar(otraLista)
         return resultado
-    
-    def __iter__(self): #defino un metodo para iterar
-        actual = self.cabeza 
-        while actual:
-            yield actual.dato  #el yield va viendo cada nodo
-            actual = actual.siguiente #recorre desde el primer al ultimo nodo
 
+    def __iter__(self):
+        self._iterador = self.cabeza
+        return self
+
+    def __next__(self):
+        if self._iterador is None:
+            raise StopIteration
+        dato = self._iterador.dato
+        self._iterador = self._iterador.siguiente
+        return dato
--- TrabajoPractico_1/problema3/modules/juegoGuerra.py
+++ TrabajoPractico_1/problema3/modules/juegoGuerra.py
@@ -1,8 +1,67 @@
-from modules.mazo import Mazo, DequeEmptyError
-from modules.carta import Carta
+# juegoGuerra.py
 import random
 
 N_TURNOS = 10000
+
+class Carta:
+    valores = {'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'J':11,'Q':12,'K':13,'A':14}
+
+    def __init__(self, valor, palo):
+        self.valor = valor
+        self.palo = palo
+    
+    def _valor_numerico(self):
+        return Carta.valores[self.valor]
+
+    def __gt__(self, otra):
+        if otra is None:
+            return True
+        return self._valor_numerico() > otra._valor_numerico()
+    
+    def __lt__(self, otra):
+        if otra is None:
+            return False
+        return self._valor_numerico() < otra._valor_numerico()
+
+    def __eq__(self, otra):
+        if otra is None:
+            return False
+        return self._valor_numerico() == otra._valor_numerico()
+
+    def __str__(self):
+        return f"{self.valor}{self.palo}"
+
+    def __repr__(self):
+        return str(self)
+
+
+class DequeEmptyError(Exception):
+    pass
+
+
+class Mazo:
+    def __init__(self):
+        self.cartas = []
+    
+    def poner_carta_arriba(self, carta):
+        self.cartas.append(carta)
+    
+    def poner_carta_abajo(self, carta):
+        self.cartas.insert(0, carta)
+    
+    def sacar_carta_arriba(self, mostrar=False):
+        if len(self.cartas) == 0:
+            raise DequeEmptyError("El mazo está vacío")
+        return self.cartas.pop()
+    
+    def __len__(self):
+        return len(self.cartas)
+    
+    def __str__(self):
+        return ', '.join(str(c) for c in reversed(self.cartas))  # para mostrar de arriba a abajo
+
+    def __repr__(self):
+        return str(self)
 
 
 class JuegoGuerra:
@@ -11,37 +70,20 @@ class JuegoGuerra:
     palos = ['♠', '♥', '♦', '♣']
     
     def __init__(self, random_seed = 0):
-        
         self._mazo_inicial = Mazo()
         self.mazo_1 = Mazo()
         self.mazo_2 = Mazo()
         self._guerra = False
         self._ganador = ''
-        self.empate = False
+        self._empate = False
         self._turno = 0
         self._cartas_en_la_mesa = []
         self._seed = random_seed
     
     @property
-    def mazo_1(self):
-        return self._mazo_1
-    
-    @mazo_1.setter
-    def mazo_1(self, valor):
-        self._mazo_1 = valor
-    
-    @property
-    def mazo_2(self):
-        return self._mazo_2
-    
-    @mazo_2.setter
-    def mazo_2(self, valor):
-        self._mazo_2 = valor
-        
-    @property
     def empate(self):
         return self._empate
-    
+        
     @empate.setter
     def empate(self, valor):
         self._empate = valor
@@ -57,131 +99,116 @@ class JuegoGuerra:
         return self._ganador
         
     def armar_mazo_inicial(self):
-        """representa el mazo inicial, este mazo se crea al inicio
-        de cada partida mediante combinación de los números y palos 
-        de la baraja para formar cartas
-        """
         random.seed(self._seed)
-        cartas = [Carta(valor, palo) for valor in JuegoGuerra.valores
-                  for palo in JuegoGuerra.palos]
-        
-        #cartas_shuffled = random.sample(cartas, len(cartas))
+        cartas = [Carta(valor, palo) for valor in JuegoGuerra.valores for palo in JuegoGuerra.palos]
         random.shuffle(cartas)
-        cartas_shuffled = cartas
-        
-        for carta in cartas_shuffled:
+        for carta in cartas:
             self._mazo_inicial.poner_carta_arriba(carta)
-        
         return self._mazo_inicial
     
     def repartir_cartas(self):
-        """reparte el mazo inicial entre los dos jugadores"""
         while len(self._mazo_inicial):
             carta_1 = self._mazo_inicial.sacar_carta_arriba()
             self.mazo_1.poner_carta_arriba(carta_1)
+            if len(self._mazo_inicial) == 0:
+                break
             carta_2 = self._mazo_inicial.sacar_carta_arriba()
             self.mazo_2.poner_carta_arriba(carta_2) 
-            
         return self.mazo_1, self.mazo_2
     
     def iniciar_juego(self, ver_partida=True):
-        
         self.armar_mazo_inicial()
         self.repartir_cartas()
-        
         self._cartas_en_la_mesa = []
         self._turno = 0
-        
-        while len(self.mazo_1) and len(self.mazo_2) and self._turno != N_TURNOS:            
+        self._guerra = False
+        self._ganador = ''
+        self._empate = False
+
+        while len(self.mazo_1) and len(self.mazo_2) and self._turno != N_TURNOS:
             try:
                 if self._guerra:
                     for _ in range(3):
                         self._cartas_en_la_mesa.append(self.mazo_1.sacar_carta_arriba())
                         self._cartas_en_la_mesa.append(self.mazo_2.sacar_carta_arriba())
                 
-                self._cartas_en_la_mesa.append(self.mazo_1.sacar_carta_arriba(mostrar=True))
-                self._cartas_en_la_mesa.append(self.mazo_2.sacar_carta_arriba(mostrar=True))
+                carta_1 = self.mazo_1.sacar_carta_arriba(mostrar=True)
+                carta_2 = self.mazo_2.sacar_carta_arriba(mostrar=True)
+
+                # chequeo para evitar None
+                if carta_1 is None or carta_2 is None:
+                    raise ValueError("Error: carta None sacada del mazo")
+
+                self._cartas_en_la_mesa.append(carta_1)
+                self._cartas_en_la_mesa.append(carta_2)
             
             except DequeEmptyError:
-                #un mazo se quedó sin cartas durante el turno
-                #declaro ganador al jugador que tiene cartas
-                #debido al orden, el jugador 1 se queda primero sin cartas
                 if len(self.mazo_1):
                     self._ganador = 'jugador 1'
                 else:
                     self._ganador = 'jugador 2'
                 self._guerra = False
                 if ver_partida:
-                    print(f'             ***** {self._ganador} gana la partida*****                           ')  
-                
+                    print(f'***** {self._ganador} gana la partida *****')
+                break
+            
             else:
                 if ver_partida:
                     self.mostrar_juego()
                
-                if  self._cartas_en_la_mesa[-2] >  self._cartas_en_la_mesa[-1]:
+                if self._cartas_en_la_mesa[-2] > self._cartas_en_la_mesa[-1]:
                     for carta in self._cartas_en_la_mesa:
                         self.mazo_1.poner_carta_abajo(carta)
                     self._cartas_en_la_mesa = []
                     self._guerra = False
                     if len(self.mazo_2):
-                        #si el mazo del oponente se queda sin cartas
-                        #el juego debe terminar sin incrementar
-                        #el número de turnos
                         self._turno += 1
-                elif  self._cartas_en_la_mesa[-1] > self._cartas_en_la_mesa[-2]:
+                elif self._cartas_en_la_mesa[-1] > self._cartas_en_la_mesa[-2]:
                     for carta in self._cartas_en_la_mesa:
                         self.mazo_2.poner_carta_abajo(carta)
                     self._cartas_en_la_mesa = []
                     self._guerra = False
-                    if len(self.mazo_1):                        
-                        #si el mazo del oponente se queda sin cartas
-                        #el juego debe terminar sin incrementar
-                        #el número de turnos
+                    if len(self.mazo_1):
                         self._turno += 1
                 else:
                     self._guerra = True
                     if ver_partida:
-                        print('                      **** Guerra!! ****                       ')
+                        print('**** Guerra!! ****')
             
-            finally:                       
-                if self._turno == N_TURNOS:
-                    self.empate = True
-                    if ver_partida:
-                        print('                       ***** Empate *****                           ')  
-                    
-        if self._turno != N_TURNOS and not self._ganador: 
+            if self._turno == N_TURNOS:
+                self.empate = True
+                if ver_partida:
+                    print('***** Empate *****')
+                break
+                      
+        
+        if self._turno != N_TURNOS and not self._ganador:
             if len(self.mazo_1):
                 self._ganador = 'jugador 1'
             else:
                 self._ganador = 'jugador 2'              
             if ver_partida:
-                print(f'           ***** {self._ganador} gana la partida*****                           ')  
-                      
-        
-        
+                print(f'***** {self._ganador} gana la partida *****')
+                
     def mostrar_juego(self):
-        # diferenciar las cartas del mazo y las 
-        # cartas en la mesa
         print(f"Turno: {self._turno+1}")
-        print('jugador 1:')        
+        print('jugador 1:')
         print(self.mazo_1)
         print()
-        print('              ', end='')
+        print('          ', end='')
         for carta in self._cartas_en_la_mesa:
             print(carta, end=' ')
         print('\n')
         print('jugador 2:')
-        print(self.mazo_2)            
+        print(self.mazo_2)
         print()
         print('------------------------------------')
         if self._ganador:
-             print(f'         ***** 3){self._ganador} gana la partida*****                           ')  
+             print(f'***** {self._ganador} gana la partida *****')
 
 
 if __name__ == "__main__":
-
     n = random.randint(0, 1000)
     juego = JuegoGuerra(random_seed=n)
     juego.iniciar_juego()
-    
-    print(n)
+    print(f'Semilla usada: {n}')
