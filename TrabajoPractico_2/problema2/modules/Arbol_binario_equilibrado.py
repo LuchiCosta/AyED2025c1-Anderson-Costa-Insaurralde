50805d1bf8d3001c888ee9b10ffc79c8cfb7aaeb
Mateo Anderson <mateo.anderson@ingenieria.uner.edu.ar>
2025-05-21 11:35:01 -0300

    Realizo el Arbol binario de busaueda equilibrado

---
+++ TrabajoPractico_2/problema2/modules/Arbol_binario_equilibrado.py
@@ -0,0 +1,137 @@
+class NodoArbol:
+    def __init__(self,clave,valor=None,izquierdo=None,derecho=None,padre=None):
+        self.clave = clave #fecha
+        self.valor = valor #temeperatura
+        self.hijoIzquierdo = izquierdo
+        self.hijoDerecho = derecho
+        self.padre = padre
+
+    def tieneHijoIzquierdo(self):
+        return self.hijoIzquierdo
+
+    def tieneHijoDerecho(self):
+        return self.hijoDerecho
+
+    def esHijoIzquierdo(self):
+        return self.padre and self.padre.hijoIzquierdo == self
+
+    def esHijoDerecho(self):
+        return self.padre and self.padre.hijoDerecho == self
+
+    def esRaiz(self):
+        return not self.padre
+
+    def esHoja(self):
+        return not (self.hijoDerecho or self.hijoIzquierdo)
+
+    def tieneAlgunHijo(self):
+        return self.hijoDerecho or self.hijoIzquierdo
+
+    def tieneAmbosHijos(self):
+        return self.hijoDerecho and self.hijoIzquierdo
+
+    def reemplazarDatoDeNodo(self,clave,valor,hizq,hder):
+        self.clave = clave
+        self.valor = valor
+        self.hijoIzquierdo = hizq
+        self.hijoDerecho = hder
+        if self.tieneHijoIzquierdo():
+            self.hijoIzquierdo.padre = self
+        if self.tieneHijoDerecho():
+            self.hijoDerecho.padre = self
+
+class ArbolBinarioEquilibrado:
+
+    def __init__(self):
+        self.raiz = None
+        self.tamano = 0
+
+    def longitud(self):
+        return self.tamano
+
+    def __len__(self):
+        return self.tamano
+
+    def agregar(self,clave,valor):
+        if self.raiz:
+            self._agregar(clave,valor,self.raiz)
+        else:
+            self.raiz = NodoArbol(clave,valor)
+        self.tamano = self.tamano + 1
+
+    def _agregar(self,clave,valor,nodoActual):
+        if clave < nodoActual.clave:
+            if nodoActual.tieneHijoIzquierdo():
+                    self._agregar(clave,valor,nodoActual.hijoIzquierdo)
+            else:
+                    nodoActual.hijoIzquierdo = NodoArbol(clave,valor,padre=nodoActual)
+                    self.actualizarEquilibrio(nodoActual.hijoIzquierdo)
+        else:
+            if nodoActual.tieneHijoDerecho():
+                    self._agregar(clave,valor,nodoActual.hijoDerecho)
+            else:
+                    nodoActual.hijoDerecho = NodoArbol(clave,valor,padre=nodoActual)
+                    self.actualizarEquilibrio(nodoActual.hijoDerecho)
+
+    def actualizarEquilibrio(self,nodo):
+        if nodo.factorEquilibrio > 1 or nodo.factorEquilibrio < -1:
+            self.reequilibrar(nodo)
+            return
+        if nodo.padre != None:
+            if nodo.esHijoIzquierdo():
+                    nodo.padre.factorEquilibrio += 1
+            elif nodo.esHijoDerecho():
+                    nodo.padre.factorEquilibrio -= 1
+
+            if nodo.padre.factorEquilibrio != 0:
+                    self.actualizarEquilibrio(nodo.padre)
+
+    def rotarIzquierda(self,rotRaiz):
+        nuevaRaiz = rotRaiz.hijoDerecho
+        rotRaiz.hijoDerecho = nuevaRaiz.hijoIzquierdo
+        if nuevaRaiz.hijoIzquierdo != None:
+            nuevaRaiz.hijoIzquierdo.padre = rotRaiz
+        nuevaRaiz.padre = rotRaiz.padre
+        if rotRaiz.esRaiz():
+            self.raiz = nuevaRaiz
+        else:
+            if rotRaiz.esHijoIzquierdo():
+                    rotRaiz.padre.hijoIzquierdo = nuevaRaiz
+            else:
+                rotRaiz.padre.hijoDerecho = nuevaRaiz
+        nuevaRaiz.hijoIzquierdo = rotRaiz
+        rotRaiz.padre = nuevaRaiz
+        rotRaiz.factorEquilibrio = rotRaiz.factorEquilibrio + 1 - min(nuevaRaiz.factorEquilibrio, 0)
+        nuevaRaiz.factorEquilibrio = nuevaRaiz.factorEquilibrio + 1 + max(rotRaiz.factorEquilibrio, 0)
+    
+    def rotarDerecha(self, rotRaiz):
+        nuevaRaiz = rotRaiz.hijoIzquierdo
+        rotRaiz.hijoIzquierdo = nuevaRaiz.hijoDerecho
+        if nuevaRaiz.hijoDerecho != None:
+            nuevaRaiz.hijoDerecho.padre = rotRaiz
+        nuevaRaiz.padre = rotRaiz.padre
+        if rotRaiz.esRaiz():
+            self.raiz = nuevaRaiz
+        else:
+            if rotRaiz.esHijoIzquierdo():
+                rotRaiz.padre.hijoIzquierdo = nuevaRaiz
+            else:
+                rotRaiz.padre.hijoDerecho = nuevaRaiz
+        nuevaRaiz.hijoDerecho = rotRaiz
+        rotRaiz.padre = nuevaRaiz
+        rotRaiz.factorEquilibrio = rotRaiz.factorEquilibrio - 1 - max(nuevaRaiz.factorEquilibrio, 0)
+        nuevaRaiz.factorEquilibrio = nuevaRaiz.factorEquilibrio - 1 + min(rotRaiz.factorEquilibrio, 0)
+
+    def reequilibrar(self,nodo):
+        if nodo.factorEquilibrio < 0:
+                if nodo.hijoDerecho.factorEquilibrio > 0:
+                    self.rotarDerecha(nodo.hijoDerecho)
+                    self.rotarIzquierda(nodo)
+                else:
+                    self.rotarIzquierda(nodo)
+        elif nodo.factorEquilibrio > 0:
+                if nodo.hijoIzquierdo.factorEquilibrio < 0:
+                    self.rotarIzquierda(nodo.hijoIzquierdo)
+                    self.rotarDerecha(nodo)
+                else:
+                    self.rotarDerecha(nodo)        
